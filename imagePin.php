228712f9f7b952ab85e4c4efb3d1524c2dc7ce39
Johan Odijk <odijk.johan@gmail.com>
2017-05-22 19:17:17 +0200

    user pin now gets moved by input

--- imagePin.php
+++ imagePin.php
@@ -1,8 +1,14 @@
-<style>
-  body {margin:0}
-</style>
 <canvas id="image">You don't have Javascript enabled! <a href="http://www.enable-javascript.com/">Click here to find out how to turn it on!</a></canvas>
+<div class="addPlace">
+  <label for="name" >Name</label><input onkeyup="changePin(this)" type="text" name='name'>
+  <label for="x">X</label><input onkeyup="changePin(this)" type="number" name='x'>
+  <label for="y">Y</label><input onkeyup="changePin(this)" type="number" name='y'>
+</div>
 <script type="text/javascript">
+  bgImg = "";
+  ctx = "";
+  pins = [];
+  pins.push(['',100,1000],['spawn',-7,178]);
   window.onload = init();
   function init() {
     /*
@@ -12,12 +18,10 @@
     bottom y: 2642
     left x: -5399
     */
-
-
-    img = getBackground("img.png"); //pic made by mineatlast
-    width = img.width;
-    height = img.height;
-
+    bgImg = getBackground("img.png"); //pic made by mineatlast
+    bgImg
+    width = bgImg.width;
+    height = bgImg.height;
     cv = document.getElementById('image');
     cv.width = width;
     cv.height = height;
@@ -25,11 +29,16 @@
     ctx = cv.getContext('2d');
     ctx.width = width;
     ctx.height = height;
-    ctx.drawImage(img,0,0);
 
-    createPin(ctx,-7,178); // spawn
+    reloadCanvas();
+
+
+  }
+
+  function reloadCanvas() {
+    ctx.clearRect(0,0,ctx.width,ctx.height);
+    ctx.drawImage(bgImg,0,0);
     loadPins(ctx);
-
 
   }
 
@@ -40,11 +49,14 @@
   }
 
   function loadPins(ctx) {
+    for (var i = 0; i < pins.length; i++) {
+      createPin(pins[i][1],pins[i][2]);
+    }
     //TODO:
     //php ajax call to get the pins from a text file and convert it to json.
   }
 
-  function createPin(ctx, mcX,mcY) {
+  function createPin(mcX,mcY) {
     //TODO:
     //make pin
     mapWidth=11904;
@@ -53,10 +65,18 @@
     yRatio = mapHeight/ctx.height;
     cvX = ctx.width/2 + mcX/xRatio;
     cvY = ctx.height/2 + mcY/yRatio;
-    console.log(cvX);
     var pinImg = new Image();
     pinImg.src = "pin.png";
     ctx.drawImage(pinImg,cvX-(pinImg.width/2),cvY-pinImg.height);
+  }
+
+  function  changePin(el) {
+    inputs = el.parentElement.querySelectorAll('input');
+    pins[0][0] = inputs[0].value;
+    pins[0][1] = inputs[1].value;
+    pins[0][2] = inputs[2].value;
+    reloadCanvas();
+    console.log(inputs[1].value);
   }
 </script>
 <?php
