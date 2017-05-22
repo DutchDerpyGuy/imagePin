c266edba6ade9f93afc8d60716ccfb379ff9e196
Johan Odijk <odijk.johan@gmail.com>
2017-05-22 17:01:19 +0200

    base init

---
+++ imagePin.php
@@ -0,0 +1,9 @@
+<canvas id="image" width="800" height="600">You don't have Javascript enabled! <a href="http://www.enable-javascript.com/">Click here to find out how to turn it on!</a></canvas>
+<script type="text/javascript">
+  cv = document.getElementById('image');
+  cv.innerHTML = '';
+</script>
+
+<?php
+
+?>
